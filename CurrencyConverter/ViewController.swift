95f164c52a53368f1f4dfe4a5676039fcd3c9a13
yongping.chen <will.yp.chen@gmail.com>
2020-05-26 21:14:38 +0900

    Initial Commit

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Yongping Chen on 2020/05/26.
+//  Copyright © 2020 Yongping Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
