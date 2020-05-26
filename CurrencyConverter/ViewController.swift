1c047e2925973eb54b6428a56a708c0a7d171452
yongping.chen <will.yp.chen@gmail.com>
2020-05-26 23:23:43 +0900

    Prototype a table view controller

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -8,11 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    let exchangeRate = [
+        "USDJPY": 107.70504,
+        "USDCNY": 7.1368,
+        "USDEUR": 0.917688
+    ]
+    let currencies =  [
+        "USD",
+        "CNY",
+        "JPY",
+        "EUR"
+    ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        self.tableView.register(<#T##cellClass: AnyClass?##AnyClass?#>, forCellReuseIdentifier: <#T##String#>)
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return currencies.count;
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellIdentifier", for: indexPath)
+        cell.textLabel?.text = currencies[indexPath.row]
+        return cell
     }
 
 
---
+++ CurrencyConverter/Views/CurrencyCellView.swift
@@ -0,0 +1,13 @@
+//
+//  CurrencyCellView.swift
+//  CurrencyConverter
+//
+//  Created by Yongping Chen on 2020/05/26.
+//  Copyright © 2020 Yongping Chen. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyCellView: UITableViewCell {
+    
+}
